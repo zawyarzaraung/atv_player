97fb9979d7c57ff670baac9efebe0ae0f1e91b94
zawyarzaraung <zawyarzaraung@gmail.com>
2026-06-11 03:30:25 +0630

    Refactor ChannelList to use Link component

--- ChannelList.js
+++ ChannelList.js
@@ -1,28 +1,16 @@
 // components/ChannelList.js
-import { useState } from "react";
-import RemoteControl from "./RemoteControl";
+import Link from "next/link";
 
 export default function ChannelList({ channels }) {
-  const [focusIndex, setFocusIndex] = useState(0);
-
-  const handleNavigate = (direction) => {
-    if (direction === "down" && focusIndex < channels.length - 1) {
-      setFocusIndex(focusIndex + 1);
-    } else if (direction === "up" && focusIndex > 0) {
-      setFocusIndex(focusIndex - 1);
-    } else if (direction === "select") {
-      window.location.href = `/player?url=${encodeURIComponent(channels[focusIndex].url)}`;
-    }
-  };
-
   return (
     <div>
       <h2>Channel List</h2>
-      <RemoteControl onNavigate={handleNavigate} />
       <ul>
         {channels.map((ch, i) => (
-          <li key={i} className={i === focusIndex ? "focused" : ""}>
-            {ch.name}
+          <li key={i}>
+            <Link href={`/player?url=${encodeURIComponent(ch.url)}`}>
+              {ch.name}
+            </Link>
           </li>
         ))}
       </ul>
