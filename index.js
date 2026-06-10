9e62a6de9af793e862b5515c497e33a32f00889f
zawyarzaraung <zawyarzaraung@gmail.com>
2026-06-11 03:06:47 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-// components/ChannelList.js
-import { useState } from "react";
-import RemoteControl from "./RemoteControl";
+// pages/index.js
+import { useState, useEffect } from "react";
+import ChannelList from "../components/ChannelList";
+import { parseM3U } from "../utils/playlistParser";
 
-export default function ChannelList({ channels }) {
-  const [focusIndex, setFocusIndex] = useState(0);
+export default function Home() {
+  const [channels, setChannels] = useState([]);
 
-  const handleNavigate = (direction) => {
-    if (direction === "down" && focusIndex < channels.length - 1) {
-      setFocusIndex(focusIndex + 1);
-    } else if (direction === "up" && focusIndex > 0) {
-      setFocusIndex(focusIndex - 1);
-    } else if (direction === "select") {
-      window.location.href = `/player?url=${encodeURIComponent(channels[focusIndex].url)}`;
-    }
-  };
+  useEffect(() => {
+    fetch("/playlist.m3u")
+      .then(res => res.text())
+      .then(text => setChannels(parseM3U(text)));
+  }, []);
 
-  return (
-    <div>
-      <h2>Channel List</h2>
-      <RemoteControl onNavigate={handleNavigate} />
-      <ul>
-        {channels.map((ch, i) => (
-          <li key={i} className={i === focusIndex ? "focused" : ""}>
-            {ch.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <ChannelList channels={channels} />;
 }
